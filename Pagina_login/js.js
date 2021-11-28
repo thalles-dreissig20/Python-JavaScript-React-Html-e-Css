573a4b1bd17c4b000f6250bc441a65f8d64bd55e
Thalles Rafael Dreissig <71471019+thalles-dreissig20@users.noreply.github.com>
2021-11-28 08:26:26 -0300

    Uma pagina de login de usuario

---
+++ Pagina_login/js.js
@@ -0,0 +1,10 @@
+function carregar() {
+    let variavel = window.document.getElementById('botao');
+    
+}
+function cadastro() {
+    window.alert('Não tem tela de cadastro ainda!')
+}
+function entrar() {
+    window.alert('Essa tela é só um exemplo de login!')
+}
