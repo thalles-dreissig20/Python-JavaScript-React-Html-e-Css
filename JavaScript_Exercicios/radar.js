1017e3abdb5df89998415dec69b9685337f7da80
Thalles Rafael Dreissig <71471019+thalles-dreissig20@users.noreply.github.com>
2021-12-06 14:25:17 -0300

    Programa de radar

---
+++ JavaScript_Exercicios/radar.js
@@ -0,0 +1,16 @@
+verificarVelocidade(50);
+
+function verificarVelocidade(velocidade){
+    const velocidadeMaxima = 70;
+    const kmPorPonto = 5;
+    if (velocidade <= velocidadeMaxima)
+        console.log('ok')
+    else{
+        const pontos = Math.floor(((velocidade - velocidadeMaxima) / kmPorPonto));
+        if (pontos >= 12)
+            console.log("Carteira suspensa");
+        else{
+            console.log("pontos",pontos);
+        }
+    }
+}
