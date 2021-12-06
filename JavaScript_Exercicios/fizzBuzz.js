6c6042cef1fd16b91d2e583bbe9754b8aa7cfa53
Thalles Rafael Dreissig <71471019+thalles-dreissig20@users.noreply.github.com>
2021-12-06 14:10:14 -0300

    Update fizzBuzz.js

--- JavaScript_Exercicios/fizzBuzz.js
+++ JavaScript_Exercicios/fizzBuzz.js
@@ -1,3 +1,9 @@
+/*
+Se i for múltiplo de 3 e 5, imprima o FizzBuzz.
+Se i for múltiplo de 3 (mas não de 5), imprima o Fizz.
+Se i for múltiplo de 5 (mas não de 3), imprima o Buzz.
+Se i não for múltiplo de 3 ou 5, imprima o valor de i.
+*/
 const resultado = fizzBuzz(5);
 console.log(resultado);
 
