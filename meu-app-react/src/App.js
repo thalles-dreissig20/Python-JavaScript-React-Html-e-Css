4f82d597d0afd75f31929904ce0a7feb2c8a6f10
Thalles Rafael Dreissig <71471019+thalles-dreissig20@users.noreply.github.com>
2021-12-08 15:32:17 -0300

    Primeiros estudos sobre react

--- meu-app-react/src/App.js
+++ meu-app-react/src/App.js
@@ -1,10 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
+
+
+import Tasks from "./componentes/Tasks.js";
 import "./App.css";
 
 const App = () => {
 
+ // let mensagem = "Hello World!";
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "programação",
+      componet: true,
+    },
+    {
+      id: "2",
+      title: "Televisão",
+      componet: false,
+    },
+    {
+      id: "2",
+      title: "Televisão",
+      componet: false,
+    },
+    {
+      id: "2",
+      title: "Televisão",
+      componet: false,
+    },
+  ]);
+
   return(
-    <div className="container"></div>
+    <>
+        <div className="container">
+          <Tasks tasks={tasks} />
+        </div>
+    </>
   )
 }
 
---
+++ meu-app-react/src/componentes/Task.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Task = () => {
+    return ( <h1>Tarefa</h1> );
+}
+ 
+export default Task;
